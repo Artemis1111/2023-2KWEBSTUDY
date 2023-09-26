5a18d9f7018d1b28770ac20ae6f290e501425ebc
Artemis1111 <chodh907@gmail.com>
2023-09-26 20:28:44 +0900

    !!

---
+++ Chapter2/index.js
@@ -0,0 +1,10 @@
+const circularShapes = require("./circular-shapes");
+const path = require("path");
+
+const r = 10;
+const h = 20;
+
+console.log(`Circumference = ${circularShapes.getCircumference(r)}`);
+console.log(`Circle Area = ${circularShapes.getCircleArea(r)}`);
+console.log(`Sphere Volume = ${circularShapes.getSphereVolume(r)}`);
+console.log(`Cylinder Surface Area = ${circularShapes.getCylinderSurfaceArea(r, h)}`);
