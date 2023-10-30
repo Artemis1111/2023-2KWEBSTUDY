3c004fc38b3b820e9e3f4770def8d605656289a3
Artemis1111 <chodh907@gmail.com>
2023-10-30 23:11:29 +0900

    4 clear

--- HW/Chapter4/Exercise1/step3.sql
+++ HW/Chapter4/Exercise1/step3.sql
@@ -1,2 +1,16 @@
 -- your code
-CREATE TABLE ~
+CREATE TABLE `courses` (
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(20) NOT NULL,
+    `student_code` INT NOT NULL, -- 학생 개별코드 
+    `entering_year` INT NOT NULL,
+    `major` INT NOT NULL, -- 단과대학 코드(2자리) + 그 속에서 전공 코드(2자리)로 구성
+    `call_number` VARCHAR(11) NOT NULL,
+    `address` VARCHAR(100) NOT NULL,
+    `accumulated_complete_credits` INT NOT NULL DEFAULT 0,
+    `average_credits` FLOAT NOT NULL DEFAULT 0.0,
+    `attendence` TINYINT(1) NOT NULL DEFAULT 1,
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- 이 때, "입학년도 + 단과대학 코드 + 학생 개별 코드"가 학번이 된다.
--- HW/Chapter4/Exercise2/messenger.sql
+++ HW/Chapter4/Exercise2/messenger.sql
@@ -1,15 +1,53 @@
 CREATE TABLE `users` (
--- Your code here
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `user_ID` VARCHAR(20) NOT NULL,
+    `user_password` VARCHAR(20) NOT NULL,
+    `nickname` VARCHAR(20) NOT NULL,
+    `propile_image_ink` VARCHAR(100) NOT NULL,
+    `propile_messege` VARCHAR(100) NOT NULL,
+    `withdraw_status` TINYINT(1) DEFAULT 0,
+    `join_date` DATETIME NOT NULL,
+    PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 CREATE TABLE `channels` (
--- Your code here
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(20) NOT NULL,
+    `user` INT NOT NULL,
+    `channel_link` VARCHAR(1000) NOT NULL,
+    `max_number_of_people` INT NOT NULL,
+    `withdraw_status` TINYINT(1) DEFAULT 0,
+    `create_channel_date` DATETIME NOT NULL,
+    PRIMARY KEY (`id`),
+    FOREIGN KEY (`user`) REFERENCES `users`(`id`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 CREATE TABLE `chats` (
--- Your code here
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `chat_user` INT NOT NULL,
+    `chat_channel` INT NOT NULL,
+    `create_chat_date` DATETIME NOT NULL,
+    PRIMARY KEY (`id`),
+    FOREIGN KEY (`chat_user`) REFERENCES `users`(`id`) ON DELETE CASCADE,
+    FOREIGN KEY (`chat_channel`) REFERENCES `channels`(`id`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-CREATE TABLE `blocks` (
--- Your code here
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 CREATE TABLE `follows` (
--- Your code here
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `follower` INT NOT NULL,
+    `followee` INT NOT NULL,
+    `follow_date` DATETIME NOT NULL,
+    PRIMARY KEY (`id`),
+    FOREIGN KEY (`follower`) REFERENCES `users`(`id`) ON DELETE CASCADE,
+    FOREIGN KEY (`followee`) REFERENCES `users`(`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `blocks` (
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `blocking_user` INT NOT NULL,
+    `blocked_user` INT NOT NULL,
+    `block_date` DATETIME NOT NULL,
+    PRIMARY KEY (`id`),
+    FOREIGN KEY (`blocking_user`) REFERENCES `users`(`id`) ON DELETE CASCADE,
+    FOREIGN KEY (`blocked_user`) REFERENCES `users`(`id`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
